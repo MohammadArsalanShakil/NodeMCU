faa807195558a1727465b9ad8ef6f9b4ae80f99a
MohammadArsalanShakil <68584194+MohammadArsalanShakil@users.noreply.github.com>
2021-04-22 21:13:28 +0500

    Add files via upload

---
+++ 4-Channel_Relay/4-Channel_Relay.ino
@@ -0,0 +1,19 @@
+#define Relay1 D1
+#define Relay2 D2
+#define Relay3 D3
+#define Relay4 D4
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(Relay1,OUTPUT);
+  pinMode(Relay2,OUTPUT);
+  pinMode(Relay3,OUTPUT);
+  pinMode(Relay4,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(Relay1, LOW);
+  digitalWrite(Relay2, LOW);
+  digitalWrite(Relay3, LOW);
+  digitalWrite(Relay4, LOW);
+}
